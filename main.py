7f22438ee237d18daef9e1258eaa6af7cd0068be
sukalaper <anggirrr31@gmail.com>
2026-05-07 13:39:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from feedgen.feed import FeedGenerator
 from datetime import datetime
 
 # Kita tembak langsung API-nya biar datanya dapet semua (gak kosong)
-api_url = "https://haji.go.id/api/berita"
+api_url = "https://haji.go.id/berita"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 }
